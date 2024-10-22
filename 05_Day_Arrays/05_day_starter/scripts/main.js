ab2699ba17ad8c30e9db9f2c62e7be74a244778e
OVSAlex15 <103588591+OVSAlex15@users.noreply.github.com>
2024-10-22 22:12:04 +0600

    Update main.js
    day 5

--- 05_Day_Arrays/05_day_starter/scripts/main.js
+++ 05_Day_Arrays/05_day_starter/scripts/main.js
@@ -1,3 +1,48 @@
 console.log(countries)
-alert('Open the browser console whenever you work on JavaScript')
-alert('Open the console and check if the countries has been loaded')
+
+
+const first_array = []
+const second_array = [1, 2, 3, 4, 5, 6]
+console.log(second_array.length)
+console.log(second_array[0], second_array[2], second_array[second_array.length-1])
+
+const mixedDataTypes = [1,'name', [1,2], {'ilya': 'Sher'}, true]
+var itCompanies = ['Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon']
+console.log(itCompanies)
+console.log(itCompanies.length)
+console.log(itCompanies[0], itCompanies[3], itCompanies[itCompanies.length-1])
+
+for (let i=0; i< itCompanies.length; i++) {
+   console.log(itCompanies[i]) 
+}
+
+for (let i=0; i< itCompanies.length; i++) {
+    var k = itCompanies[i]
+    console.log(k.toUpperCase())
+ }
+
+
+
+let company = 'Ether'
+let index = itCompanies.indexOf(company)
+
+if (index === -1){
+    console.log('the company is not found')
+} else{
+    console.log(company)
+}
+
+itCompanies.sort()
+console.log(itCompanies)
+itCompanies.reverse()
+console.log(itCompanies)
+
+
+console.log(itCompanies.slice(0,2))
+console.log(itCompanies.slice(itCompanies.length-4, itCompanies.length-1))
+console.log(itCompanies.slice(3))
+
+console.log(itCompanies.splice(0,1))
+console.log(itCompanies.splice(3,4))
+console.log(itCompanies.splice(itCompanies.length-4, itCompanies.length-1))
+console.log(itCompanies.splice())
